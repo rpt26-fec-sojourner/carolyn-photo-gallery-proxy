06ee615bb8340bf09a7727c64f1bc41214ecac53
Carolyn Tran <carolyntran953@gmail.com>
2021-03-22 19:58:43 -0500

    create App.jsx file, add cors response header

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Proxy extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(?, ?);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/1');
+});
+
+app.get('/:id', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
