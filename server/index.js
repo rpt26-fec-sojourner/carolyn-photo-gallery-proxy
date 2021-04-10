b56d68c8402f74c34732b68532bfb4492421b289
Carolyn Tran <carolyntran953@gmail.com>
2021-04-10 17:16:56 -0500

    create navbar

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
